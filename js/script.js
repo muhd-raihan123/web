f14bbd1f2fd44f9c6696df5ec324680eaf336b56
muhd-raihan123 <muhammad_22011767@utp.edu.my>
2025-06-05 18:50:39 +0800

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function downloadCV() {
+  window.open("pdf/CV_MOHD_RAIHAN.pdf", "_blank");
+}
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
+
+function back() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
